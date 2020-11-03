a9dca12e02a1d4fdd041e4c3fb37e1e5db639335
Mirek Simek <miroslav.simek@vscht.cz>
2020-11-03 09:01:41 +0100

    docs

--- library/router.js
+++ library/router.js
@@ -146,6 +146,80 @@ export function collection(
   return proto
 }
 
+/**
+ * Wraps a component and injects collectionApi to its props. The collectionApi is configured with the current collection.
+ *
+ * @param options                 options
+ * @param {string} options.apiUrl          Invenio api url, defaults to '/api'
+ * @param {string} options.collectionCode  collection code that will be appended to api url. If not defined, equals to route path
+ * @param {string} options.path            route path. If not defined, equals to collectionCode
+ * @param {Component} options.component
+ *                                wrapped component
+ *
+ * @param {string} options.recordRouteName
+ *      name of route that will be used to show the record. the route must contain ``recordId`` url param.
+ * @param {InvenioHttpOptionOptions} options.httpOptionsProps
+ *      InvenioHttpOptionOptions used for fetching collection options
+ * @param {InvenioCollectionListOptions} options.httpGetProps
+ *      InvenioCollectionListOptions used for fetching the collection
+ *
+ * @param extra                   extra parameters to be merged with the route object
+ *
+ * @returns object to put into the router routes
+ *
+ * ```html
+ * <component
+ *   :is="currentComponent"
+ *   :collection-api="collectionApi"
+ *   :records="collectionApi.records.value"
+ *   :facets="facets"
+ *   :loading="collectionApi.loading.value"
+ *   :loaded="collectionApi.loaded.value"
+ *   :error="collectionApi.error.value"
+ *   :stale="collectionApi.stale.value"
+ *   :pages="collectionApi.pages.value"
+ *   :reload="collectionApi.reload"
+ * ></component>
+ * ```
+ *
+ * *Example*:
+ *
+ * ```javascript
+ *  routes = [
+ *    collectionApi({
+ *      path: '/create'
+ *      collectionCode: 'records',
+ *      component: RecordCreator,
+ *      },
+ *      { name: 'create' }
+ *    )
+ *  ]
+ * ```
+ *
+ *  Will evaluate to:
+ *
+ * ```javascript
+ *  routes = [
+ *   {
+ *      path: '/create',
+ *      component: CollectionApiWrapper,
+ *      name: 'create',
+ *      props: {
+ *        collectionCode: 'records',
+ *        viewerComponent: RecordCreator,
+ *      },
+ *      meta: {
+ *        query: {
+ *          page: 'int:1',
+ *          size: 'int:10',
+ *          facets: 'commaarray:',
+ *          q: 'string:'
+ *        }
+ *      }
+ *    }
+ *  ]
+ * ```
+ */
 export function collectionApi(
   {
     path,
